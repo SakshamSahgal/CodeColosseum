03a50f3dbb4ebe72f9c6f6475841b50a4ccf9720
SakshamSahgal <sakshamsahgal5@gmail.com>
2024-12-29 01:20:03 +0530

    Add theme selection functionality to Submission component; integrate ThemeFooter for user theme preferences

--- Frontend/src/Pages/Submission.js
+++ Frontend/src/Pages/Submission.js
@@ -7,10 +7,18 @@ import CodeMirror from '@uiw/react-codemirror';
 import StdoutPallet from '../Components/StdoutPallet';
 import CompilerOutputPallet from '../Components/CompilerOutputPallet';
 import SubmissionCard from '../Components/Submission/SubmissionCard';
+import ThemeFooter from '../Components/CodeEditor/ThemeFooter';
+
+
 function Submission() {
   const { submissionToken, email } = useParams();
   const [submission, setSubmission] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [theme, setTheme] = useState('default');
+
+  const handleThemeChange = (theme) => {
+    setTheme(theme);
+  }
 
   useEffect(() => {
     makeApiRequest({
@@ -42,20 +50,24 @@ function Submission() {
       <Container className='mt-5'>
         {submission ? (<>
           <SubmissionCard submission={submission} />
-          <CompilerOutputPallet compile_output={submission.compile_output} />
-          <StdoutPallet stdout={submission.stdout} />
           <Card className="mt-3">
-            <Card.Header className="text-center">
+            <Card.Header className="text-center bg-dark text-light">
               <h3>Code</h3>
             </Card.Header>
             <Card.Body>
               <CodeMirror
                 value={submission.source_code ? submission.source_code : ''}
+                theme={theme === 'default' ? undefined : theme}
                 height="500px"
                 editable={false}
               />
             </Card.Body>
+            <Card.Footer className="text-center bg-dark text-light">
+              <ThemeFooter theme={theme} handleThemeChange={handleThemeChange} />
+            </Card.Footer>
           </Card>
+          <CompilerOutputPallet compile_output={submission.compile_output} />
+          <StdoutPallet stdout={submission.stdout} />
         </>
         ) : (
           <Alert variant="warning" className="text-center shadow-sm p-4 mt-4">
